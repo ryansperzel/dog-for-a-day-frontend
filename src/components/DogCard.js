666801780e5e6fb852d56ef7f44cb63e733697e9
Ryan Sperzel <ryansperzel@gmail.com>
2017-11-21 22:07:19 -0500

    Added hover style to Dog cards

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
+import { Route } from "react-router-dom";
+import Home from './Home'
 import '../index.css'
+import { withRouter } from 'react-router-dom'
+
 
 export default class DogList extends Component {
 
 
+  handleClick = (event) => {
+    event.preventDefault()
+    console.log(event.target.parentNode)
+  }
+
   render() {
     return(
-      
-      <div className="card">
-        <img className="cardpic" alt="" src={this.props.dog.media.photos.photo[3].$t} />
-        <div className="container">
-          <h4><b>{this.props.dog.name.$t}</b></h4>
-          <p></p>
-        </div>
-      </div>
+
+        <a onClick={this.handleClick} href="" className="textWithBlurredBg">
+          <img className="dog-image" alt="" src={this.props.dog.media.photos.photo[3].$t} />
+          <h2>{this.props.dog.name.$t}</h2>
+          <h1></h1>
+        </a>
+
     )
   }
 }
